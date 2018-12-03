d5e9ab868322a4fc491f5549355563873c68e002
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-12-03 13:12:37 -0600

    update

---
+++ EnterLowercaseLetters/EnterLowercaseLetters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace EnterLowercaseLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter;
+            string inputString;
+            WriteLine("Enter A letter: ");
+         
+            letter = Convert.ToChar(inputString);
+           
+            if(letter >= 'a' && letter <= 'z')
+            {
+                WriteLine("OK");
+            }
+
+        }
+    }
+}
