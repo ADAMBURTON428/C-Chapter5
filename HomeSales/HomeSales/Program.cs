cd7e2ab099cafb1a1abc0ef9e79a2092913f3c67
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-12-05 13:01:58 -0600

    update

---
+++ HomeSales/HomeSales/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace HomeSales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double danilleTotal = 0;
+            double edwardTotal = 0;
+            double francisTotal = 0;
+            string input = null;
+
+
+            while (input != "Z" || input != "z")
+            {
+
+
+
+                WriteLine("Input SalesPerson initial ");
+                input = ReadLine();
+               
+                if ("F" == input || "f" == input)
+                {
+                    double saleHome = 0;
+                    francisTotal = saleHome + francisTotal;
+                    WriteLine(francisTotal);
+                }
+                else if ("D" == input || "d" == input)
+                {
+                    double saleHome = 0;
+                    danilleTotal = saleHome + danilleTotal;
+                    WriteLine(danilleTotal);
+                }
+                else if ("E" == input || "e" == input)
+                {
+                    double saleHome = 0;
+                    edwardTotal = saleHome + edwardTotal;
+                    WriteLine(edwardTotal);
+                }
+                else
+                    WriteLine(edwardTotal);
+                    WriteLine(danilleTotal);
+                    WriteLine(francisTotal);
+            }
+
+            
+        }
+    }
+}
--- TestScores/TestScores/Program.cs
+++ TestScores/TestScores/Program.cs
@@ -10,22 +10,36 @@ namespace TestScores
     {
         static void Main(string[] args)
         {
+            const int MAX = 100;
+            const int MIN = 0;
             int ScoreNumber = 0;
             int Test = 0;
             string TestString;
             int average = 0;
-
+            int Total= 0;
             while (Test != 999)
             {
-                WriteLine("Enter a Test Score>>>>");
+
+                WriteLine("Enter a Test Score or 999 to exit >>>>");
                 TestString = ReadLine();
                 Test = Convert.ToInt32(TestString);
-                ScoreNumber++;
-                average = Test / (ScoreNumber);
-               
-            }
+                if (Test > MIN && Test < MAX)
+                {
+                    Total = Total + Test;
+                    ScoreNumber++;
+                       
+                   
+                }
+                else if (Test == 999)
+                {
+                    average = Total / ScoreNumber;
+                    Write("The average is " + average + " out of " + ScoreNumber + " Scores");
 
-            Write("The average is " + (average - 999));
+
+                }
+            }
+             
+            
         }
     }
 }
